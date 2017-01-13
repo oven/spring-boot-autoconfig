9028988180fe115d091d8b20111946bd1af8d2fa
Ove Gram Nipen <ove@nipen.no>
2017-01-13 21:32:00 +0100

    løsning på oppgave 2

--- autoconfig/src/main/java/no/bouvet/autoconfig/ParkingClientAutoConfiguration.java
+++ autoconfig/src/main/java/no/bouvet/autoconfig/ParkingClientAutoConfiguration.java
@@ -1,4 +1,10 @@
 package no.bouvet.autoconfig;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ParkingClientAutoConfiguration {
+    public ParkingClientAutoConfiguration() {
+        System.out.println("\n\n***** Configuring parking client\n\n");
+    }
 }
