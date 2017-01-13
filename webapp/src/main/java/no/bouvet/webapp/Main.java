a623913b90defa422b816dab04ac16e3d3d3432e
Ove Gram Nipen <ove@nipen.no>
2017-01-13 21:01:10 +0100

    simplified main

--- webapp/src/main/java/no/bouvet/webapp/Main.java
+++ webapp/src/main/java/no/bouvet/webapp/Main.java
@@ -2,20 +2,9 @@ package no.bouvet.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-@EnableConfigurationProperties
-public class Main extends SpringBootServletInitializer implements WebApplicationInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
-
+public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
