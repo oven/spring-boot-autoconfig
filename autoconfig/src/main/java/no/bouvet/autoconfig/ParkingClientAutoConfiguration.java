ed48f68d778864b7d096ab87d777040e490a48b8
Ove Gram Nipen <ove@nipen.no>
2017-01-14 21:13:36 +0100

    løsning på oppgave 3

--- autoconfig/src/main/java/no/bouvet/autoconfig/ParkingClientAutoConfiguration.java
+++ autoconfig/src/main/java/no/bouvet/autoconfig/ParkingClientAutoConfiguration.java
@@ -1,10 +1,21 @@
 package no.bouvet.autoconfig;
 
+import no.bouvet.parking.ParkingClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
 
 @Configuration
 public class ParkingClientAutoConfiguration {
     public ParkingClientAutoConfiguration() {
         System.out.println("\n\n***** Configuring parking client\n\n");
+    }
+
+    @Bean
+    public ParkingClient parkingClient() {
+        ParkingClient parkingClient = new ParkingClient();
+        parkingClient.setEndpoint(URI.create("https://www.bergen.kommune.no/wsproxy/parkering.json"));
+        return parkingClient;
     }
 }
--- webapp/src/main/java/no/bouvet/webapp/MyController.java
+++ webapp/src/main/java/no/bouvet/webapp/MyController.java
@@ -1,10 +1,24 @@
 package no.bouvet.webapp;
 
+import no.bouvet.parking.CarParkStatus;
+import no.bouvet.parking.ParkingClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class MyController {
+
+    @Autowired
+    ParkingClient parkingClient;
+
+    @GetMapping("/parking")
+    public CarParkStatus[] parking() throws IOException {
+        return parkingClient.getData();
+    }
+
     @GetMapping
     public String hello() {
         return "hello, world!";
